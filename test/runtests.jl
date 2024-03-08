0c83032c17540542001f9aee2c51c1493790a9a2
DrWatson <no@mail>
2024-03-08 00:48:12 +0000

    File setup by DrWatson

---
+++ test/runtests.jl
@@ -0,0 +1,17 @@
+using DrWatson, Test
+@quickactivate "TL-FWPI"
+
+# Here you include files using `srcdir`
+# include(srcdir("file.jl"))
+
+# Run test suite
+println("Starting tests")
+ti = time()
+
+@testset "TL-FWPI tests" begin
+    @test 1 == 1
+end
+
+ti = time() - ti
+println("\nTest took total time of:")
+println(round(ti/60, digits = 3), " minutes")
